835d702fd02a6a2e1bcfb1f0482d6102abb2bee9
tharun0728 <tharunbheesetty0728@gmail.com>
2025-02-11 22:58:49 +0530

    fix routing issue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,8 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const basename = "/Portfolio";
 root.render(
-  <React.StrictMode basename={basename}>
-    <App />
+  <React.StrictMode>
+    <BrowserRouter basename="/Portfolio">   {/* Apply basename here */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
